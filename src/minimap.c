2d40b6d532e217785b1fa2e0995c26a2f31b4853
Sayd Romey <sayd@null.net>
2024-03-11 11:45:25 -0400

    round minimap test, needs better centering

--- src/minimap.c
+++ src/minimap.c
@@ -6,7 +6,7 @@
 /*   By: cdumais <cdumais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 13:35:48 by cdumais           #+#    #+#             */
-/*   Updated: 2024/03/10 21:29:50 by cdumais          ###   ########.fr       */
+/*   Updated: 2024/03/11 11:44:08 by cdumais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ t_minimap	init_minimap(t_cub *cub)
 	// 
 	mini.img = new_img(cub->mlx, WIDTH / 3, WIDTH / 3, true);
 	mini.img->instances->x = WIDTH - mini.img->width; //place on the right
+	// 
+	mini.round_img = new_img(cub->mlx, WIDTH / 3, WIDTH / 3, true);
 	// 
 	mini.tile_size = 42;
 	// 
@@ -67,6 +69,30 @@ static void	draw_tile(mlx_image_t *img, t_point origin, t_point size, int color)
 	}
 }
 
+static void	draw_tiles(t_minimap *minimap, t_point start, t_point end)
+{
+	int		x;
+	int		y;
+	t_point	tile;
+	t_point	size;
+
+	size.x = minimap->tile_size - 1;
+	size.y = minimap->tile_size - 1;
+	y = start.y;
+	while (y < end.y)
+	{
+		x = start.x;
+		while (x < end.x)
+		{
+			tile.x = (x - start.x) * minimap->tile_size;
+			tile.y = (y - start.y) * minimap->tile_size;
+			draw_tile(minimap->img, tile, size, tile_color(y, x));
+			x++;
+		}
+		y++;
+	}
+}
+
 /* ************************************************************************** */
 
 /* maybe put in libft? */
@@ -105,43 +131,58 @@ static void	calculate_bounds(t_point center, t_point *start, t_point *end)
 	end->y = center.y + half_height;
 }
 
-static void	draw_tiles(t_minimap *minimap, t_point start, t_point end)
-{
-	int		x;
-	int		y;
-	t_point	tile;
-	t_point	size;
+/* ************************************************************************** */ //circle map
 
-	size.x = minimap->tile_size - 1;
-	size.y = minimap->tile_size - 1;
-	y = start.y;
-	while (y < end.y)
+static bool	is_in_circle(t_point point, t_point center, int radius)
+{
+	t_point	distance;
+
+	distance.x = point.x - center.x;
+	distance.y = point.y - center.y;
+	return ((distance.x * distance.x + distance.y * distance.y) <= radius * radius);
+	// return (pow(point.x - center.x, 2) + pow(point.y - center.y, 2)) <= pow(radius, 2);
+	
+}
+
+static void	make_it_round(t_minimap *minimap, t_map *map)
+{
+	int		radius = minimap->round_img->width / 3;
+	t_point	circle_center = {radius, radius};
+
+	int	map_width = map->width * minimap->tile_size;
+	int	map_height = map->height * minimap->tile_size;
+
+	t_fpoint	pos = call_cub()->player.position;
+
+	int	center_offset_x = (int)pos.x - radius;
+	int	center_offset_y = (int)pos.y - radius;
+
+	center_offset_x = ft_clamp(center_offset_x, 0, map_width - minimap->round_img->width);
+	center_offset_y = ft_clamp(center_offset_y, 0, map_height - minimap->round_img->height);
+
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < (int)minimap->round_img->height)
 	{
-		x = start.x;
-		while (x < end.x)
+		x = 0;
+		while (x < (int)minimap->round_img->width)
 		{
-			tile.x = (x - start.x) * minimap->tile_size;
-			tile.y = (y - start.y) * minimap->tile_size;
-			draw_tile(minimap->img, tile, size, tile_color(y, x));
+			if (is_in_circle((t_point){x, y}, circle_center, radius))
+			{
+				int	map_x = center_offset_x + x;
+				int	map_y = center_offset_y + y;
+
+				int	color = get_pixel(minimap->img, map_x, map_y);
+				
+				draw_pixel(minimap->round_img, x, y, color);
+			}
 			x++;
 		}
 		y++;
 	}
 }
-
-/* ************************************************************************** */ //iso?
-
-
-/* ************************************************************************** */ //circle map
-
-// static bool	is_in_circle(t_point point, t_point center, int radius)
-// {
-// 	t_point	distance;
-
-// 	distance.x = point.x - center.x;
-// 	distance.y = point.y - center.y;
-// 	return ((distance.x * distance.x + distance.y * distance.y) <= radius * radius);
-// }
 
 /* ************************************************************************** */ //features (options)
 
@@ -166,120 +207,36 @@ static void	features_testing(t_minimap *minimap, t_map *map)
 
 /* ************************************************************************** */
 
+void	draw_mini_player(t_minimap *minimap, t_player *player)
+{
+	t_fpoint	line_size;
+	int			ray_length = 42; //tmp
+	int			ray_color = 0x00FF00FF; //tmp
+
+	line_size.x = player->position.x + player->delta.x * ray_length;
+	line_size.y = player->position.y + player->delta.y * ray_length;
+	draw_line(minimap->img, player->position, line_size, ray_color);
+	draw_circle(minimap->img, player->position, player->size, player->color);
+}
+
 void	draw_minimap(t_minimap *minimap, t_map *map)
 {
 	t_point	start;
 	t_point	end;
 
 	clear_img(minimap->img);
+	clear_img(minimap->round_img);
 	if (call_cub()->keys.m)
 	{
 		features_testing(minimap, map);
 		calculate_bounds(find_center(minimap, map), &start, &end);
 		draw_tiles(minimap, start, end);
+		draw_mini_player(minimap, &call_cub()->player);
+
+		make_it_round(minimap, map);
 	}
-	
-	
 }
 
-/*
-tests for more intricate minimap display
-*/
-
-// void	draw_round_minimap(mlx_image_t *main_img, mlx_img_t *minimap_img, t_fpoint minimap_center, int radius)
-// {
-// 		int	x;
-// 		int	y;
-// 		int	distance_squared;
-// 		int color;
-
-// 		y = 0;
-// 		while (y < minimap_img->height)
-// 		{
-// 			y++;
-// 			x = 0;
-// 			while (x < minimap_img->width)
-// 			{
-// 				distance_squared = (x - minimap_center.x) * (x - minimap_center.x) + (y - minimap_center.y) * (y - minimap_center.y);
-				
-// 				if (distance_squared <= radius * radius)
-// 				{
-// 					// pixel is in circle, draw from minimap
-// 					color = get_pixel(minimap_img, x, y);
-// 					draw_pixel(main_img, x + minimap_center.x - radius, y + minimap_center.y - radius, color);
-// 				}
-// 				else
-// 				{
-// 					// pixel is outside the circle, draw from main image
-// 					// not necessary if main img is already drawn and the minimap is drawn over it
-// 				}
-// 				x++;
-// 			}
-// 		}
-// }
-
-/*
-1. prepare the large map image
-
-set enable = false;
-
-2. create minimap image, will serve as minimap overlay
-where we will draw the portion of the large map image centered around the player's current pos
-
-3. update minimap based on player movement
-*do we have a different struct 't_miniplayer' ?
-
-when the player moves (hooks)
-	- calculate the source coordinates
-	based on the player's position on the large map, calculate top left coord of the portion of the large map img we want to display on the minimap
-	(centered around the player's position)
-	
-	- copy pixels from large image to minimap image (using the circular effect to copy the source pixels from large map if in circle)
-
-	- draw if boolean is toggled
-*/
-
-// void	render_minimap(mlx_image_t *full_map_img, mlx_image_t *minimap_img, mlx_image_t *main_img, t_player player, int radus)
-// {
-//     int sourceX = player.x - minimap_img->width / 2;
-//     int sourceY = player.y - minimap_img->height / 2;
-
-//     // Adjust sourceX and sourceY if they go out of bounds of the large_map_img
-
-//     for (int y = 0; y < minimap_img->height; y++) {
-//         for (int x = 0; x < minimap_img->width; x++) {
-//             int largeMapX = sourceX + x;
-//             int largeMapY = sourceY + y;
-
-//             // Copy pixel from large_map_img to minimap_img, or from main_img if outside the circular area
-//             int color;
-//             if (is_inside_circle(x, y, radius)) {
-//                 color = get_pixel(large_map_img, largeMapX, largeMapY);
-//             } else {
-//                 color = get_pixel(main_img, largeMapX, largeMapY); // Adjust coordinates as needed
-//             }
-//             draw_pixel(minimap_img, x, y, color);
-//         }
-//     }
-
-//     // Optionally, draw minimap_img onto the main window
-// }
-
-
-
-	// // assuming the minimap is a square
-	// t_fpoint	center;
-
-	// center.x = minimap_img->width / 2;
-	// center.y = minimap_img->height / 2;
-
-	// // [...]while y<height, while x<width
-	// if (is_inside_circle((t_fpoint){x, y}, center, radius))
-	// 	// the point(x, y) is inside the circle
-	// else
-	// 	// it is outside the circle
-
-/* ************************************************************************** */
 
 
 /* maybe use this version for minimap ?
@@ -335,17 +292,7 @@ when the player moves (hooks)
 /* ************************************************************************** */
 /* ************************************************************************** */
 
-// void	draw_player(mlx_image_t *img, t_player *player)
-// {
-// 	t_fpoint	line_size;
-// 	int		ray_length = 42; //tmp
-// 	int		ray_color = 0x00FF00FF; //tmp
 
-// 	line_size.x = player->position.x + player->delta.x * ray_length;
-// 	line_size.y = player->position.y + player->delta.y * ray_length;
-// 	draw_line(img, player->position, line_size, ray_color);
-// 	draw_circle(img, player->position, player->size, player->color);
-// }
 
 // void	draw_player2(mlx_image_t *img, t_player *player)
 // {

70b788cdae27f22e89259f1cd708349e09946d43
Cederic Dumais <cdumais@c2r3p1.42quebec.com>
2024-04-22 12:27:52 -0400

    mergerererer

--- src/__todo.c
+++ src/__todo.c
@@ -6,7 +6,7 @@
 /*   By: cdumais <cdumais@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 23:31:55 by cdumais           #+#    #+#             */
-/*   Updated: 2024/04/22 11:45:46 by cdumais          ###   ########.fr       */
+/*   Updated: 2024/04/22 12:27:06 by cdumais          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,44 +17,23 @@ add 'if (BONUS)' checks for functions that are not in mandatory part:
 	mouse control,
 	...
 
-Complete parsing, then isolate in 
-
 **!! currently segfaults if a map has no elevator (in normal mode, did not test in bonus mode).
 
+
+
+**************************************************************************
+Parsing:
 
 implement parsing and verification of elevator in parse_map()
 ** (make a list of what is valid, what is not, edge cases, etc..)
 
 **************************************************************************
-
 Minimap:
 
-add enable/disable controls
-
-clamp on the edges or use margin ??**
-
-make tests with rotating the minimap instead of the player
-make tests with iso minimap
-draw a ring around it ?
+t.v. static effect when in elevator?
 
 **************************************************************************
 Elevator:
-
-TODO:
-	cleanup elevator (img) (change when oli chooses how to do the buttons)
-
-*ideas*
-	do we implement a waiting period
-	elevator music ?
-
-	we also need booleans for:
-		
-		- is player inside (to allow for level change)
-			
-			*! cue elevator music ? (should we adjust music volume with player's proximity and cut it when doors are closed ?)
-
-incorporate parsing in previous functions to liberate the elevator code
-
 
 **************************************************************************
 
@@ -68,10 +47,6 @@ create tutorial or instruction menu ? (mouse for clicking on elevator buttons)
 **************************************************************************
 Segworld:
 
-
-if one of the levels does not have an elevator,
-and the player tries to get to it through an elevator,
-we go into segworld, and the elevator breaks... TUN TUN TUNNNNH !!!
 
 - add flickering lights, shadow effect
 
